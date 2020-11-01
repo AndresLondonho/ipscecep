43b7f6d1abcdec180bc7e33c53390d69850a0b66
JulianInternationalCorp <ipiajulian4@gmail.com>
2020-11-01 13:09:07 -0500

    Ubi fcecep

--- index.php
+++ index.php
@@ -380,7 +380,7 @@
             3. Click "Share" and choose "Embed map" tab
             4. Copy only URL and paste it within the src="" field below
 	-->
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3647.3030413476204!2d100.5641230193719!3d13.757206847615207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf51ce6427b7918fc!2sG+Tower!5e0!3m2!1sen!2sth!4v1510722015945" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+          <iframe src="https://www.google.com/maps/place/fcecep/@3.429653,-76.5374846,17z/data=!3m1!4b1!4m5!3m4!1s0x8e30a690d035b647:0xe28267e34f932e45!8m2!3d3.4296476!4d-76.5352959?hl=es" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
      </section>           
 
 
