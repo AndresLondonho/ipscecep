65d3cd563576e0f4563e39f04937a57ab6f2b0d5
AndresLondonho <andreslondonho412@gmail.com>
2020-11-09 21:34:42 -0500

    commit

--- ips_cecep.sql
+++ ips_cecep.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2020 a las 02:14:10
+-- Tiempo de generación: 10-11-2020 a las 03:32:57
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.6
 
@@ -49,14 +49,21 @@ INSERT INTO `cargo` (`id_cargo`, `nom_cargo`) VALUES
 
 CREATE TABLE `cita` (
   `nro_cita` int(10) NOT NULL,
-  `id_cargo` int(4) NOT NULL,
-  `id_rol` int(4) NOT NULL,
-  `valor_cita` bigint(6) NOT NULL,
+  `id_func` bigint(10) NOT NULL,
   `id_pac` bigint(10) NOT NULL,
   `id_sede` int(4) NOT NULL,
-  `id_serv` int(4) NOT NULL,
-  `fecha_hora` datetime NOT NULL
+  `id_espec` int(4) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `Detalle` text COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `cita`
+--
+
+INSERT INTO `cita` (`nro_cita`, `id_func`, `id_pac`, `id_sede`, `id_espec`, `fecha`, `hora`, `Detalle`) VALUES
+(6, 101, 1112486444, 1011, 1, '2020-11-09', '16:40:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -85,19 +92,20 @@ INSERT INTO `ciudad` (`id_ciu`, `nom_ciu`, `id_pais`) VALUES
 
 CREATE TABLE `especialidad` (
   `id_espec` int(4) NOT NULL,
-  `nom_espec` varchar(20) COLLATE utf8_spanish_ci NOT NULL
+  `nom_espec` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `derivacion` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `especialidad`
 --
 
-INSERT INTO `especialidad` (`id_espec`, `nom_espec`) VALUES
-(1, 'Medico General'),
-(2, 'Odontologo'),
-(3, 'Ginecologo'),
-(4, 'Oftalmologo'),
-(5, 'Traumatologo');
+INSERT INTO `especialidad` (`id_espec`, `nom_espec`, `derivacion`) VALUES
+(1, 'Medicina General', 'Medico General'),
+(2, 'Odontologia', 'Odontologo(a)'),
+(3, 'Ginecologia', 'Ginecologo(a)'),
+(4, 'Oftalmologia', 'Oftalmologo(a)'),
+(5, 'Traumatologia', 'Traumatologo(a)');
 
 -- --------------------------------------------------------
 
@@ -106,13 +114,15 @@ INSERT INTO `especialidad` (`id_espec`, `nom_espec`) VALUES
 --
 
 CREATE TABLE `funcionarios` (
-  `id_func` int(4) NOT NULL,
+  `id_func` bigint(10) NOT NULL,
   `id_priv` int(4) NOT NULL,
   `username` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `nom_user` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `nom2_user` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
   `ape_user` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
-  `tel_user` int(10) NOT NULL,
+  `ape2_user` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `tel_user` bigint(10) NOT NULL,
   `cc_user` bigint(10) NOT NULL,
   `email_user` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `id_cargo` int(4) DEFAULT NULL,
@@ -125,37 +135,13 @@ CREATE TABLE `funcionarios` (
 -- Volcado de datos para la tabla `funcionarios`
 --
 
-INSERT INTO `funcionarios` (`id_func`, `id_priv`, `username`, `password`, `nom_user`, `ape_user`, `tel_user`, `cc_user`, `email_user`, `id_cargo`, `id_espec`, `id_sede`, `img_user`) VALUES
-(101, 2, 'medico1', 'ytfrcyjvguyr', 'Cristian', 'Londono', 1234567, 1234894, 'yoquese@menos.com', 2, 3, 1011, 'cristian.jpg'),
-(102, 2, 'medico2', 'asaefcdv cdaff', 'Rafael', 'Loaiza', 5154861, 84512354, 'asdvausb@dufhs.com', 2, 1, 1011, 'andres.jpg');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `medico`
---
-
-CREATE TABLE `medico` (
-  `id_med` int(11) NOT NULL,
-  `cc_med` int(15) NOT NULL,
-  `nom_med` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
-  `ape_med` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
-  `tel_med` bigint(20) NOT NULL,
-  `sede` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `espec` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
-  `img_med` varchar(30) COLLATE utf8_spanish2_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `medico`
---
-
-INSERT INTO `medico` (`id_med`, `cc_med`, `nom_med`, `ape_med`, `tel_med`, `sede`, `espec`, `img_med`) VALUES
-(1, 1112486446, 'Rafael Andres', 'Londono Loaiza', 3182665156, 'Cali', 'Medico General', 'andres.jpg'),
-(2, 1005828611, 'Cristian David', 'Loaiza Estrada', 3017505543, 'Palmira', 'Odontologo', 'cristian.jpg'),
-(3, 1007352231, 'Esteban', 'Penaranda Campo', 3128705098, 'Candelaria', 'Ginecologo', 'esteban.jpg'),
-(4, 1005872140, 'Julian', 'Ipia Capote', 3116547336, 'Pance', 'Oftalmologo', 'unknown.jpg'),
-(5, 1006048640, 'Sebastian', 'Idrobo Castaneda', 3003983213, 'Guacarí', 'Traumatologo', 'sebastian.jpg');
+INSERT INTO `funcionarios` (`id_func`, `id_priv`, `username`, `password`, `nom_user`, `nom2_user`, `ape_user`, `ape2_user`, `tel_user`, `cc_user`, `email_user`, `id_cargo`, `id_espec`, `id_sede`, `img_user`) VALUES
+(101, 2, 'medico1', 'ytfrcyjvguyr', 'Cristian', 'David', 'Loaiza', 'Estrada', 3017505543, 1234894, 'yoquese@menos.com', 2, 3, 1011, 'cristian.jpg'),
+(102, 2, 'medico2', '8548413165651', 'rafael', '', 'Londono', '', 5154861, 33265154, 'asdsda@gmail.com', 2, 1, 1011, NULL),
+(103, 2, 'iwachu', 'ufaisdjbvyasvfbwñevbiñsubvuñsbvIWACHU', 'rafael', 'David', 'Londono', 'Estrada', 1234567, 12348945, 'asdvausb@dufhs.com', 1, NULL, 1011, NULL),
+(4444, 2, 'jtiro', '4444', 'Jhin', 'Cuarto', 'Tiro', 'Sublime', 4444444, 4444, 'elvirtuoso@cuatro.com', 2, 1, 1011, ''),
+(5151, 2, 'andres', '123456', 'Rafael', 'Andres', 'Londono', 'Loaiza', 3182665156, 1112486446, 'andreslondonho@gmail.com', 2, 1, 1011, ''),
+(5152, 2, 'rlondono', '1112486447', 'Rafael', 'David', 'Londono', 'Loaiza', 3017505543, 1112486447, 'andres@gmail.com', 2, 4, 1011, '');
 
 -- --------------------------------------------------------
 
@@ -164,7 +150,7 @@ INSERT INTO `medico` (`id_med`, `cc_med`, `nom_med`, `ape_med`, `tel_med`, `sede
 --
 
 CREATE TABLE `pacientes` (
-  `id_pac` int(4) NOT NULL,
+  `id_pac` bigint(10) NOT NULL,
   `nom_pac` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `ape_pac` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `cc_pac` bigint(10) NOT NULL,
@@ -179,7 +165,7 @@ CREATE TABLE `pacientes` (
 --
 
 INSERT INTO `pacientes` (`id_pac`, `nom_pac`, `ape_pac`, `cc_pac`, `email_pac`, `tel_pac`, `dir_pac`, `id_ciu`) VALUES
-(1, 'rafael', 'loaiza', 123456789, 'jfytf@hjhb.com', 3215465, 'cra34#54-98', 1001);
+(1112486444, 'rafael', 'loaiza', 1112486444, 'jfytf@hjhb.com', 3215465, 'cra34#54-98', 1001);
 
 -- --------------------------------------------------------
 
@@ -239,7 +225,7 @@ CREATE TABLE `sede` (
 --
 
 INSERT INTO `sede` (`id_sede`, `nom_sede`, `dir_sede`, `tel_sede`, `id_ciu`, `id_rol`) VALUES
-(1011, 'Champagnat', 'Cl. 9b #29a67', 3828282, 1001, NULL);
+(1011, 'Champagnat', 'Cl. 9b #29a67', 3828282, 1001, 103);
 
 -- --------------------------------------------------------
 
@@ -251,6 +237,13 @@ CREATE TABLE `servicios` (
   `id_serv` int(4) NOT NULL,
   `nom_serv` varchar(20) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`id_serv`, `nom_serv`) VALUES
+(1, 'odontologia');
 
 --
 -- Índices para tablas volcadas
@@ -267,11 +260,10 @@ ALTER TABLE `cargo`
 --
 ALTER TABLE `cita`
   ADD PRIMARY KEY (`nro_cita`),
-  ADD KEY `id_cargo` (`id_cargo`),
-  ADD KEY `id_rol` (`id_rol`),
+  ADD KEY `id_rol` (`id_func`),
   ADD KEY `id_pac` (`id_pac`),
   ADD KEY `id_sede` (`id_sede`),
-  ADD KEY `id_serv` (`id_serv`);
+  ADD KEY `id_serv` (`id_espec`);
 
 --
 -- Indices de la tabla `ciudad`
@@ -296,12 +288,6 @@ ALTER TABLE `funcionarios`
   ADD KEY `id_priv` (`id_priv`),
   ADD KEY `id_espec` (`id_espec`),
   ADD KEY `id_sede` (`id_sede`);
-
---
--- Indices de la tabla `medico`
---
-ALTER TABLE `medico`
-  ADD PRIMARY KEY (`id_med`);
 
 --
 -- Indices de la tabla `pacientes`
@@ -345,19 +331,7 @@ ALTER TABLE `servicios`
 -- AUTO_INCREMENT de la tabla `cita`
 --
 ALTER TABLE `cita`
-  MODIFY `nro_cita` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `medico`
---
-ALTER TABLE `medico`
-  MODIFY `id_med` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `pacientes`
---
-ALTER TABLE `pacientes`
-  MODIFY `id_pac` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `nro_cita` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
@@ -367,10 +341,9 @@ ALTER TABLE `pacientes`
 -- Filtros para la tabla `cita`
 --
 ALTER TABLE `cita`
-  ADD CONSTRAINT `cita_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `funcionarios` (`id_func`),
-  ADD CONSTRAINT `cita_ibfk_2` FOREIGN KEY (`id_cargo`) REFERENCES `cargo` (`id_cargo`),
-  ADD CONSTRAINT `cita_ibfk_3` FOREIGN KEY (`id_serv`) REFERENCES `servicios` (`id_serv`),
-  ADD CONSTRAINT `cita_ibfk_4` FOREIGN KEY (`id_pac`) REFERENCES `pacientes` (`cc_pac`),
+  ADD CONSTRAINT `cita_ibfk_1` FOREIGN KEY (`id_func`) REFERENCES `funcionarios` (`id_func`),
+  ADD CONSTRAINT `cita_ibfk_3` FOREIGN KEY (`id_espec`) REFERENCES `especialidad` (`id_espec`),
+  ADD CONSTRAINT `cita_ibfk_4` FOREIGN KEY (`id_pac`) REFERENCES `pacientes` (`id_pac`),
   ADD CONSTRAINT `cita_ibfk_5` FOREIGN KEY (`id_sede`) REFERENCES `sede` (`id_sede`);
 
 --
@@ -398,8 +371,7 @@ ALTER TABLE `pacientes`
 -- Filtros para la tabla `sede`
 --
 ALTER TABLE `sede`
-  ADD CONSTRAINT `sede_ibfk_1` FOREIGN KEY (`id_ciu`) REFERENCES `ciudad` (`id_ciu`),
-  ADD CONSTRAINT `sede_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `funcionarios` (`id_func`);
+  ADD CONSTRAINT `sede_ibfk_1` FOREIGN KEY (`id_ciu`) REFERENCES `ciudad` (`id_ciu`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
